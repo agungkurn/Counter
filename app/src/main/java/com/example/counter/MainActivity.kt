1258904841c875e78cee1716fc980edec6cbe2a0
agungkurn <agung.excurive@live.com>
2019-08-23 10:18:59 +0000

    Adding stopwatch feature

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.counter
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -10,6 +8,10 @@ import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        internal val TAG = MainActivity::class.java.simpleName
+    }
+
     private val viewModel by lazy {
         ViewModelProviders.of(this)[MainViewModel::class.java]
     }
@@ -17,7 +19,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setSupportActionBar(bar)
 
         viewModel.getValue().observe(this, Observer {
@@ -25,21 +26,25 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return if (item.itemId == R.id.menu_reset) {
-            tv_count.text = "0"
-            true
-        } else {
-            false
-        }
-    }
-
     fun incrementValue(v: View) {
         viewModel.incrementValue(tv_count.text.toString().toInt())
+    }
+
+    fun resetValue(v: View) {
+        tv_count.text = "0"
+    }
+
+    fun controlStopwatch(v: View) {
+        viewModel.getStopwatchValue().observe(this, Observer {
+            fab_stopwatch.text = it
+        })
+        viewModel.isStopwatchStarted().observe(this, Observer { started ->
+            if (started) {
+                fab_stopwatch.icon = getDrawable(R.drawable.ic_stop_black_24dp)
+            } else {
+                fab_stopwatch.icon = getDrawable(R.drawable.ic_play_arrow_black_24dp)
+            }
+        })
+        viewModel.controlStopwatch()
     }
 }
--- app/src/main/java/com/example/counter/MainViewModel.kt
+++ app/src/main/java/com/example/counter/MainViewModel.kt
@@ -1,15 +1,75 @@
 package com.example.counter
 
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainViewModel : ViewModel() {
-    private val countValue = MutableLiveData<Int>()
+    // Coroutines put in here
+    private val viewModelJob = SupervisorJob()
+    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
 
-    internal fun getValue(): LiveData<Int> = countValue
+    // LiveDatas put in here
+    private val countRepValue = MutableLiveData<Int>()
+    private val stopwatchValue = MutableLiveData<String>()
+    private val isStopwatchStarted = MutableLiveData<Boolean>()
+
+    // Object vars put in here
+    private val formatter = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+
+    // Standard vars put in here
+    private var secondInMillis = 0L
+
+    // Getters put in here
+    internal fun getValue(): LiveData<Int> = countRepValue
+
+    internal fun getStopwatchValue(): LiveData<String> = stopwatchValue
+    internal fun isStopwatchStarted(): LiveData<Boolean> = isStopwatchStarted
 
     internal fun incrementValue(displayedValue: Int) {
-        countValue.value = displayedValue + 1
+        countRepValue.value = displayedValue + 1
+    }
+
+    internal fun controlStopwatch() {
+        val cal = Calendar.getInstance()
+        cal.set(Calendar.HOUR_OF_DAY, 0)
+        cal.set(Calendar.MINUTE, 0)
+        cal.set(Calendar.SECOND, 0)
+        cal.set(Calendar.MILLISECOND, 0)
+
+        var handler: Handler? = null
+
+        if (isStopwatchStarted.value == true) {
+            handler = null
+            isStopwatchStarted.value = false
+        }
+
+        if (handler == null) {
+            isStopwatchStarted.value = true
+            handler = Handler(Looper.getMainLooper())
+            handler.postDelayed(object : Runnable {
+                override fun run() {
+                    val currentTime =
+                        SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(cal.time)
+                    stopwatchValue.postValue(currentTime)
+                    Log.d(MainActivity.TAG, "Current time: $currentTime")
+                    handler.postDelayed(this, 1000)
+                    cal.add(Calendar.SECOND, 1)
+                }
+            }, 10)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
